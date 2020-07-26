6f24628ac3de92f8c24f5612714d6e50601b7471
screendragons <chiyuyeung@outlook.com>
2020-07-26 20:28:50 +0200

    First commit

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>About Nuxt.js</h1>
+      <p>This in an app that displays</p>
+  </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'About us',
+            meta: {
+                hid: 'description',
+                name: 'description',
+                content: 'Best place for Nuxt.js content'
+                
+
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
